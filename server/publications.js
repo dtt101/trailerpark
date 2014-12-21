618a6425434062b2115d1e24895e8333e4a81453
David Thompson <dtt101@gmail.com>
2014-12-21 18:19:49 +0000

    Denormalise comments count

--- server/publications.js
+++ server/publications.js
@@ -1,1 +1,1 @@
-Meteor.publish('posts', function() {  return Posts.find();});Meteor.publish('comments', function() {  return Comments.find();})
+Meteor.publish('posts', function() {  return Posts.find();});Meteor.publish('comments', function(postId) {  check(postId, String);  return Comments.find({postId: postId});})
