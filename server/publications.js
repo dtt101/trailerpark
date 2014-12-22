b04df7e70c9cee902b91d7bdd70bef5372fa1fed
David Thompson <dtt101@gmail.com>
2014-12-22 21:33:23 +0000

    Only publish notifications relevant to current user

--- server/publications.js
+++ server/publications.js
@@ -1,1 +1,1 @@
-Meteor.publish('posts', function() {  return Posts.find();});Meteor.publish('comments', function(postId) {  check(postId, String);  return Comments.find({postId: postId});});Meteor.publish('notifications', function() {  return Notifications.find();})
+Meteor.publish('posts', function() {  return Posts.find();});Meteor.publish('comments', function(postId) {  check(postId, String);  return Comments.find({postId: postId});});Meteor.publish('notifications', function() {  return Notifications.find({userId: this.userId, read: false});})
