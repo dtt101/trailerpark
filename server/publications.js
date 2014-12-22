3e34718d3529a03bf47a1926afae3284bfc585bf
David Thompson <dtt101@gmail.com>
2014-12-22 21:22:51 +0000

    Add notifications

--- server/publications.js
+++ server/publications.js
@@ -1,1 +1,1 @@
-Meteor.publish('posts', function() {  return Posts.find();});Meteor.publish('comments', function(postId) {  check(postId, String);  return Comments.find({postId: postId});})
+Meteor.publish('posts', function() {  return Posts.find();});Meteor.publish('comments', function(postId) {  check(postId, String);  return Comments.find({postId: postId});});Meteor.publish('notifications', function() {  return Notifications.find();})
