aabab579f7ec5591335d262755a7757af9e3521b
David Thompson <dtt101@gmail.com>
2014-12-31 14:51:26 +0000

    Add voting button to post listing

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,1 +1,1 @@
-Template.postItem.helpers({  ownPost: function() {    return this.userId === Meteor.userId();  },  domain: function() {    var a = document.createElement('a');    a.href = this.url;    return a.hostname;  }  });
+Template.postItem.helpers({  ownPost: function() {    return this.userId === Meteor.userId();  },  domain: function() {    var a = document.createElement('a');    a.href = this.url;    return a.hostname;  }});Template.postItem.events({  'click .upvote': function(e) {    e.preventDefault();    Meteor.call('upvote', this._id);  }})
