1c27579f838baab74717efaae47448460e520b04
David Thompson <dtt101@gmail.com>
2015-02-08 09:27:36 +0000

    Add velocity testing framework to Meteor

---
+++ tests/mocha/server/sampleServerTest.js
@@ -0,0 +1,19 @@
+// server-side tests
+if (!(typeof MochaWeb === 'undefined')) {
+  MochaWeb.testOnly(function() {
+
+    describe("Server initialization", function(){
+      it("should have a Meteor version defined", function(){
+        chai.assert(Meteor.release);
+      });
+
+      it("should insert posts into the database after server start", function(){
+        chai.assert(Posts.find().count() > 0);
+      });
+    });
+
+
+
+
+  });
+}
