6f6b127f039f7989f2799085580d43128eaa88e3
David Thompson <dtt101@gmail.com>
2015-02-08 09:33:13 +0000

    Add a test for trailer listing

--- tests/mocha/client/sampleClientTest.js
+++ tests/mocha/client/sampleClientTest.js
@@ -1,8 +1,8 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
-    describe("a group of tests", function(){
-      it("should respect equality", function(){
-        chai.assert.equal(5,5);
+    describe("Trailer listing", function(){
+      it("should show ten trailers on first load", function(){
+        chai.assert.equal(Posts.find().count() , 10);
       });
     });
   });
