38c653baeb073ae6580e1a7926b1def59564dc22
David Thompson <dtt101@gmail.com>
2015-02-08 11:40:05 +0000

    Test paging posts

--- tests/mocha/client/homepage.js
+++ tests/mocha/client/homepage.js
@@ -2,8 +2,19 @@ if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
     describe("Trailer listing", function(){
       it("should show ten trailers on first load", function(){
-        chai.assert.equal(Posts.find().count() , 10);
+        chai.assert.equal(Posts.find().count(), 10);
       });
     });
+
+    describe("Paging", function() {
+      it("should load ten more posts when pressing next page", function() {
+        setTimeout((function() {
+          $('a.load-more').click();
+          chai.assert.equal(Posts.find().count(), 20);
+          return done();
+        }), 100);
+      });
+    });
+
   });
 }
